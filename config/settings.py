f33f793f28ab09f68c1acc2e726a62c1fa44648b
Blagovest Nedkov <purechromas@gmail.com>
2023-11-11 01:08:02 +0500

    docstring

--- config/settings.py
+++ config/settings.py
@@ -48,5 +48,5 @@ cipher_suite = Fernet(KEY)
 scheduler = AsyncIOScheduler()
 scheduler.add_job(
     find_expired_secrets,
-    CronTrigger(hour=23, minute=0, second=0, timezone=timezone.utc),
+    CronTrigger(hour=24, minute=0, second=0, timezone=timezone.utc),
 )
